0c7b57e328e365bdfa57e19d2d6100ab97d44e01
bqanderson <bqanderson@me.com>
2016-06-28 10:26:47 -0500

    Style changes to form, try new fuctionality.

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 $(function(){
   var carsList = [];
-  $('form').on("submit", function(event){
+  var carNum = 0;
+  $('#carInfo').on("submit", function(event){
     event.preventDefault();
-    var serialized = $('form').serializeArray;
-    console.log($('form').serializeArray());
-    // carsList.push(serialized);
+    var serialized = $('#carInfo').serializeArray();
+    carsList.push(serialized);
+    carNum = carsList.length-1;
+    $('.cars').append('<p>' + carsList[carNum][0]['make'] + '</p>')
+    console.log(carsList);
+
   });
 
+  // function Car(make, model, year, color, rating, price){
+  //   this.make = make;
+  //   this.model = model;
+  //   this.year = year;
+  //   this.color = color;
+  //   this.rating = rating;
+  //   this.price = price;
+// }
 });
