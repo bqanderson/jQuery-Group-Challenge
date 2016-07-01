a6ce408f2f07d65e908acc805a98a95a90fab4ef
tracyvincent <tracykvincent@gmail.com>
2016-06-30 21:10:07 -0500

    finishes functionality, basic styling

--- app.js
+++ app.js
@@ -6,11 +6,19 @@ $(function(){
     var serialized = $('#carInfo').serializeObject();
     carsList.push(serialized);
     carNum = carsList.length-1;
-    $('.cars').append('<div class="rate1"><p>' + carsList[carNum].make + '</p></div>')
+    $('.cars').append('<div class="rate' + carsList[carNum].rating + '"><h3>Car ' + carsList.length + '</h3><ul><li>Make: ' + carsList[carNum].make + '</li>' +
+    '<li>Model: ' + carsList[carNum].model + '</li><li>Year: ' + carsList[carNum].year + '</li>' +
+    '<li>Color: ' + carsList[carNum].color + '</li><li>Customer Rating: ' + carsList[carNum].rating + '</li>' +
+    '<li>Price: ' + carsList[carNum].price + '<button class="remove">Remove</button></li></div>');
+    $('#carInfo').trigger("reset");
     console.log(carsList);
 
   });
 
+  $('.cars').on('click', '.remove', function () {
+    $(this).closest("div").remove();
+});
+});
   // function Car(make, model, year, color, rating, price){
   //   this.make = make;
   //   this.model = model;
@@ -19,4 +27,3 @@ $(function(){
   //   this.rating = rating;
   //   this.price = price;
 // }
-});
