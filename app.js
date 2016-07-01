afdb365a837dc47704c5ef1277784e6d5059be04
bqanderson <bqanderson@me.com>
2016-07-01 13:34:53 -0500

    Update stylesheet.css and app.js

--- app.js
+++ app.js
@@ -6,17 +6,20 @@ $(function(){
     var serialized = $('#carInfo').serializeObject();
     carsList.push(serialized);
     carNum = carsList.length-1;
-    $('.cars').append('<div class="rate1"><p>' + carsList[carNum].make + '</p></div>')
+    carsList[carNum].price = parseInt(carsList[carNum].price);
+    $('.cars').append('<div class="rate' + carsList[carNum].rating + '"><h3>' +
+    carsList[carNum].make + ' ' + carsList[carNum].model + '</h3><ul>' +
+    '<li>Year: ' + carsList[carNum].year + '</li>' +
+    '<li>Color: ' + carsList[carNum].color +
+    '</li><li>Customer Rating: ' + carsList[carNum].rating +
+    '</li>' + '<li>Price: ' + carsList[carNum].price.toLocaleString('en-US', {style: 'currency', currency: 'USD'}) +
+    '<button class="remove">Remove</button></li></div>');
+    $('#carInfo').trigger("reset");
     console.log(carsList);
 
   });
 
-  // function Car(make, model, year, color, rating, price){
-  //   this.make = make;
-  //   this.model = model;
-  //   this.year = year;
-  //   this.color = color;
-  //   this.rating = rating;
-  //   this.price = price;
-// }
+  $('.cars').on('click', '.remove', function () {
+    $(this).closest("div").remove();
+});
 });
